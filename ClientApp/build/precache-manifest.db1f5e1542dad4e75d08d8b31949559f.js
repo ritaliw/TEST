729aaed403c8e20ac39b93b41405b573c992d8be
Ritali Warade <ritaliw@maqsoftware.com>
2023-01-18 13:18:47 +0530

    first commit

---
+++ ClientApp/build/precache-manifest.db1f5e1542dad4e75d08d8b31949559f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7f1b84c4fc364557bcab1c111bcaf97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26c0f504c248434669bc",
+    "url": "/static/css/2.d7b63610.chunk.css"
+  },
+  {
+    "revision": "0d8880ffcc2829abec71",
+    "url": "/static/css/main.1502e656.chunk.css"
+  },
+  {
+    "revision": "26c0f504c248434669bc",
+    "url": "/static/js/2.a621950e.chunk.js"
+  },
+  {
+    "revision": "0665cc432fdf5ae3f3433075c30e3f5e",
+    "url": "/static/js/2.a621950e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0d8880ffcc2829abec71",
+    "url": "/static/js/main.c6658210.chunk.js"
+  },
+  {
+    "revision": "3c97f22164f99a6b2624",
+    "url": "/static/js/runtime-main.187efde1.js"
+  },
+  {
+    "revision": "876d6c05e9575e6bebaebd04bafab868",
+    "url": "/static/media/search_icon.876d6c05.png"
+  }
+]);
---
+++ ClientApp/build/precache-manifest.dcd38b7bf6bf1c2bbc006de5d7bf5726.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2ae32fa19101710cf24fb538d2140d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26c0f504c248434669bc",
+    "url": "/static/css/2.d7b63610.chunk.css"
+  },
+  {
+    "revision": "4b178b18f0eecd9f5c60",
+    "url": "/static/css/main.1502e656.chunk.css"
+  },
+  {
+    "revision": "26c0f504c248434669bc",
+    "url": "/static/js/2.a621950e.chunk.js"
+  },
+  {
+    "revision": "0665cc432fdf5ae3f3433075c30e3f5e",
+    "url": "/static/js/2.a621950e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b178b18f0eecd9f5c60",
+    "url": "/static/js/main.fe9214b4.chunk.js"
+  },
+  {
+    "revision": "3c97f22164f99a6b2624",
+    "url": "/static/js/runtime-main.187efde1.js"
+  },
+  {
+    "revision": "876d6c05e9575e6bebaebd04bafab868",
+    "url": "/static/media/search_icon.876d6c05.png"
+  }
+]);
